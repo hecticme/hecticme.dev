cb12fb91afde00938ad1792ed7ea3a3a121c1415
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-03-30 14:56:59 +0700

    chore: remove `@` absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '~': fileURLToPath(new URL('./src', import.meta.url)),
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
