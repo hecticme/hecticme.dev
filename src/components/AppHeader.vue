12eb932e636e46debbea33e0eaccb359031bac85
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-03-31 00:13:58 +0700

    feat: build AppHeader component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+import { routes } from '~/router'
+</script>
+
+<template>
+  <header class="border-b border-b-neutral-700">
+    <div class="size-container py-4 flex justify-between gap-2 text-sm font-medium">
+      <div class="flex gap-6">
+        <RouterLink
+          v-for="(item, index) of routes"
+          :key="index"
+          class="text-zinc-500 hover:text-zinc-300 transition-colors"
+          :to="item.path"
+        >
+          {{ item.label }}
+        </RouterLink>
+      </div>
+
+      <div class="flex gap-6">
+        <button class="text-zinc-500 hover:text-zinc-300 transition-colors">
+          <Icon icon="ph:sun-duotone" />
+        </button>
+      </div>
+    </div>
+  </header>
+</template>
