9ff24e571e634cf39721afb98876612cdf94e8db
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-04-25 00:12:44 +0700

    style: don't apply size-container to main content by default

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import AppFooter from '~/components/AppFooter.vue'
   <div class="bg-zinc-100 dark:bg-zinc-900 text-zinc-700 dark:text-zinc-300 flex flex-col min-h-screen">
     <AppHeader />
 
-    <main class="size-container py-10 flex-1">
+    <main class="py-10 flex-1">
       <RouterView />
     </main>
 
