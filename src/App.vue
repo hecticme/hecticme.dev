080135beef4c034a764dc72d7d86831ac85f6425
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-04-24 23:13:08 +0700

    style: add footer to default layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import AppHeader from '~/components/AppHeader.vue'
+import AppFooter from '~/components/AppFooter.vue'
 </script>
 
 <template>
-  <div class="bg-zinc-100 dark:bg-zinc-900 text-zinc-700 dark:text-zinc-300 min-h-screen">
+  <div class="bg-zinc-100 dark:bg-zinc-900 text-zinc-700 dark:text-zinc-300 flex flex-col min-h-screen">
     <AppHeader />
 
-    <main class="size-container py-10">
+    <main class="size-container py-10 flex-1">
       <RouterView />
     </main>
+
+    <AppFooter />
   </div>
 </template>
