39f288daaf9577f476e9aa8833dce96f8981a6ab
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-04-24 23:57:05 +0700

    feat: add sidebar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 import { routes } from '~/router'
 
+import { Icon } from '@iconify/vue'
 import ThemeToggler from '~/components/ThemeToggler.vue'
+import AppSidebar from '~/components/AppSidebar.vue'
+
+const isOpen = ref(false)
+function toggleSidebar(shouldOpen?: boolean) {
+  isOpen.value = typeof shouldOpen === 'boolean'
+    ? shouldOpen
+    : !isOpen.value
+}
+
+const router = useRouter()
+router.afterEach(() => {
+  toggleSidebar(false)
+})
 </script>
 
 <template>
@@ -12,16 +28,29 @@ import ThemeToggler from '~/components/ThemeToggler.vue'
           v-for="(item, index) of routes"
           :key="index"
           active-class="text-zinc-700 dark:text-zinc-300"
-          class="text-zinc-500 hover:text-zinc-700 dark:hover:text-zinc-300 transition-colors"
+          class="hidden md:inline text-zinc-500 hover:text-zinc-700 dark:hover:text-zinc-300 transition-colors"
           :to="item.path"
         >
           {{ item.label }}
         </RouterLink>
+
+        <button
+          aria-label="Open Sidebar"
+          class="md:hidden"
+          @click="toggleSidebar()"
+        >
+          <Icon icon="ph:list-bold" />
+        </button>
       </div>
 
       <div class="border-x border-zinc-300 dark:border-zinc-700 border-dashed px-4 py-4 flex gap-6 bg-zinc-100 dark:bg-zinc-900">
         <ThemeToggler />
       </div>
     </div>
+
+    <AppSidebar
+      :is-open="isOpen"
+      @toggle-sidebar="toggleSidebar"
+    />
   </header>
 </template>
---
+++ src/components/AppSidebar.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { routes } from '~/router'
+import { Icon } from '@iconify/vue'
+
+const props = defineProps({
+  isOpen: {
+    type: Boolean,
+    required: true,
+  },
+})
+
+const emit = defineEmits([
+  'toggleSidebar',
+])
+</script>
+
+<template>
+  <Teleport to="#teleports">
+    <Transition>
+      <div
+        v-if="props.isOpen"
+        class="fixed inset-0 z-20 overflow-y-auto px-12 py-8 flex flex-col gap-10 bg-zinc-100 dark:bg-zinc-900 text-zinc-700 dark:text-zinc-300"
+      >
+        <div class="flex justify-end">
+          <button
+            aria-label="Close Sidebar"
+            type="button"
+            @click="emit('toggleSidebar')"
+          >
+            <Icon
+              class="w-6 h-6"
+              icon="ph:x"
+            />
+          </button>
+        </div>
+
+        <div class="flex flex-col gap-4">
+          <RouterLink
+            v-for="(item, index) of routes"
+            :key="index"
+            active-class="text-zinc-700 dark:text-zinc-300"
+            class="text-zinc-500 hover:text-zinc-700 dark:hover:text-zinc-300 transition-colors"
+            :to="item.path"
+          >
+            {{ item.label }}
+          </RouterLink>
+        </div>
+      </div>
+    </Transition>
+  </Teleport>
+</template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: transform 250ms var(--default-transition-timing-function);
+}
+
+.v-enter-from,
+.v-leave-to {
+  transform: translateX(-100%);
+}
+</style>
