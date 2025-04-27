08b55426aee23744d9e31ebe69bd0665cb140fb7
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-04-27 19:56:16 +0700

    chore: add copyright text to footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="border-t border-t-zinc-300 dark:border-t-zinc-700 border-dashed bg-prototype">
     <div class="size-container border-x border-zinc-300 dark:border-zinc-700 border-dashed px-6 py-2 bg-zinc-100 dark:bg-zinc-900">
-      ...Footer...
+      <span>© 2025 H. Ngọc Minh</span>
     </div>
   </footer>
 </template>
