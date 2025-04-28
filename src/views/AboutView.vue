d5af3ad2334d6f59431c1114408f2d3e7dc3c6a6
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-04-28 22:56:22 +0700

    feat: build `/about` page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,36 @@
+<script setup lang="ts">
+import AppContacts from '~/components/AppContacts.vue'
+</script>
+
 <template>
-  <div>
-    About
+  <div class="size-prose">
+    <h1 class="text-3xl sm:text-4xl font-semibold tracking-tight">
+      Hello!
+    </h1>
+
+    <h2 class="mt-5 text-xl sm:text-2xl font-semibold tracking-tight">
+      I'm Minh.
+    </h2>
+
+    <div class="mt-9 flex flex-col gap-4">
+      <p>
+        I do web development for a living, mostly on the Frontend. Driven by my obsession with detail, I'm passionate
+        about crafting experiences that are engaging, intuitive and accessible.
+      </p>
+
+      <p>
+        I don't have a specific goal at the moment. I'm simply focused on improving my development skills.
+      </p>
+
+      <p>
+        Outside of programming, you will most likely see me playing video games. If not, I'm probably just enjoying some
+        alone time. The silent helps calm my mind.
+      </p>
+    </div>
+
+    <div class="mt-9 flex flex-col gap-2">
+      <p>You can find me on these platforms</p>
+      <AppContacts />
+    </div>
   </div>
 </template>
