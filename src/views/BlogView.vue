80f8edfb15541e58a45994ffe4709e2e6066b861
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-04-29 18:36:28 +0700

    Build blog page

---
+++ src/views/BlogView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="size-prose">
+    <h1 class="text-3xl sm:text-4xl font-semibold tracking-tight">
+      Blog
+    </h1>
+
+    <p>Oops! There's nothing here yet.</p>
+  </div>
+</template>
