abbdfb936bdeb276e1ee05931ef4706be0a9b464
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-04-30 18:20:41 +0700

    style: add per-route transition

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,35 @@ import AppFooter from '~/components/AppFooter.vue'
     <AppHeader />
 
     <main class="py-10 flex-1">
-      <RouterView />
+      <RouterView v-slot="{ Component }">
+        <Transition
+          mode="out-in"
+          name="fade"
+        >
+          <component :is="Component" />
+        </Transition>
+      </RouterView>
     </main>
 
     <AppFooter />
   </div>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition-property: opacity, translate;
+  transition-duration: 150ms;
+  transition-timing-function: var(--default-transition-timing-function);
+}
+
+.fade-enter-from {
+  translate: 0 -0.25rem;
+  opacity: 0;
+}
+
+.fade-leave-to {
+  translate: 0 0.25rem;
+  opacity: 0;
+}
+</style>
