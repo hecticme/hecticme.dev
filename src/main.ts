6a185f72f3ceec17417f8da8000a9d1ba9c2c564
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-05-04 09:50:24 +0700

    chore: use unhead in the vue app

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
+import { createHead } from '@unhead/vue/client'
 
 const app = createApp(App)
 
 app.use(router)
+
+const head = createHead()
+app.use(head)
 
 app.mount('#app')
